e25d0519aefa36c068989fabd13a803a8991f206
Sakif <sakifworkspace@gmail.com>
2025-02-27 17:38:24 +0600

    add Todo Schema and Todo Routers

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose')
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://hridoy:sdh0612xtrm@cluster0.djg6r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(()=>console.log('Connection Successful'))
+    .catch(err=>console.log(err))
+
+function errorHandler(err,req,res,next){
+    if(res.headersSent){
+        return next(err)
+    }
+    res.status(500).json({error:err})
+}
+
+app.get('/',(req,res)=>{
+res.send("This is Home")
+})
+
+app.listen(3000,()=>{
+    console.log("app is listening on port 3000")
+})
